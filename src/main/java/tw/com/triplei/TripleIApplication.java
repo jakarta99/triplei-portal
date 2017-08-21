9f1378b15bd59038c05f4e915e1c53194ae72f94
Gary Lee <jakarta99@gmail.com>
2017-08-21 16:17:51 +0800

    initialize project tripleI as SpringBootApplication.

---
+++ src/main/java/tw/com/triplei/TripleIApplication.java
@@ -0,0 +1,15 @@
+package tw.com.triplei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TripleIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleIApplication.class, args);
+
+	}
+
+}
