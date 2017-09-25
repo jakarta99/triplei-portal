31b68afe130e4146fcc4d4f4a7ce3277bda64eef
LAPTOP-0SE5S3IT\monkey <monkey81817@hotmail.com>
2017-09-25 16:02:51 +0800

    商品訂單 controller

--- src/main/java/tw/com/triplei/admin/AdminRecipientController.java
+++ src/main/java/tw/com/triplei/admin/AdminRecipientController.java
@@ -39,64 +39,69 @@ import tw.com.triplei.service.RecipientService;
 @Controller
 @RequestMapping("/admin/recipient")
 public class AdminRecipientController {
-	
+
 	@Autowired
 	private UserDao userDao;
-	
+
 	@Autowired
 	private ConvenienceStoreDao convenienceStoreDao;
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private RecipientService recipientService;
-	
+
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String listPage(Model model) {
 		return "/admin/recipient/recipientList";
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String editPage(@PathVariable("id") final Long id, Model model) {
-		
+
 		RecipientEntity dbEntity = recipientService.getOne(id);
 		model.addAttribute("entity", dbEntity);
-		
+
 		return "/admin/recipient/recipientEdit";
 	}
-	
+
 	@GetMapping
 	@ResponseBody
-	public GridResponse<RecipientEntity> query(final Model model, final RecipientEntity form, @RequestParam("pageIndex") int pageIndex, @RequestParam("pageSize") int pageSize) {
+	public GridResponse<RecipientEntity> query(final Model model, final RecipientEntity form,
+			@RequestParam("pageIndex") int pageIndex, @RequestParam("pageSize") int pageSize) {
 		Pageable pageable = new PageRequest(pageIndex - 1, pageSize);
-		
+
 		Page<RecipientEntity> page;
 
-		
 		try {
 
-//			final List<AzaleaCriterion> criterions = Lists.newArrayList();
-//
-//
-//			if (!Strings.isNullOrEmpty(form.getAccount())) {
-//				criterions.add(new AzaleaCriterion(QueryOpType.LIKE, "account", form.getAccount() + "%"));
-//			}
-//
-//			if (!Strings.isNullOrEmpty(form.getLocalName())) {
-//				criterions.add(new AzaleaCriterion(QueryOpType.LIKE, "localName", "%" + form.getLocalName() + "%"));
-//			}
-//
-//			if (form.getStatus() != null) {
-//				criterions.add(new AzaleaCriterion(QueryOpType.EQ, "status", form.getStatus()));
-//			}
+			// final List<AzaleaCriterion> criterions = Lists.newArrayList();
+			//
+			//
+			// if (!Strings.isNullOrEmpty(form.getAccount())) {
+			// criterions.add(new AzaleaCriterion(QueryOpType.LIKE, "account",
+			// form.getAccount() + "%"));
+			// }
+			//
+			// if (!Strings.isNullOrEmpty(form.getLocalName())) {
+			// criterions.add(new AzaleaCriterion(QueryOpType.LIKE, "localName",
+			// "%" + form.getLocalName() + "%"));
+			// }
+			//
+			// if (form.getStatus() != null) {
+			// criterions.add(new AzaleaCriterion(QueryOpType.EQ, "status",
+			// form.getStatus()));
+			// }
 
 			// adminRole 可以管理所有的通路和使用者，userAdminRole 僅可以管理自己 ROOT_ID 之下的通路和使用者
-//			if (!RoleUtil.isHaveAdminRoles()) {
-//				final SecUser loginUser = (SecUser) SecurityUtils.getSubject().getPrincipal();
-//				criterions.add(new AzaleaCriterion(QueryOpType.EQ, "rootId", loginUser.getRootChannelId()));
-//			}
-			
+			// if (!RoleUtil.isHaveAdminRoles()) {
+			// final SecUser loginUser = (SecUser)
+			// SecurityUtils.getSubject().getPrincipal();
+			// criterions.add(new AzaleaCriterion(QueryOpType.EQ, "rootId",
+			// loginUser.getRootChannelId()));
+			// }
+
 			page = recipientService.getAll(new RecipientSpecification(), pageable);
 		} catch (final Exception e) {
 			return new GridResponse<>(e);
@@ -104,26 +109,24 @@ public class AdminRecipientController {
 
 		return new GridResponse<>(page);
 	}
-	
+
 	@PostMapping
-	public AjaxResponse<RecipientEntity> insert(final Model model
-			,@RequestParam("name") String name
-			,@RequestParam("age") String age,@RequestParam("gender") String gender
-			,@RequestParam("tel") String tel,@RequestParam("date_1_1") String date_1_1
-			,@RequestParam("date_1_2") String date_1_2,@RequestParam("date_1_3") String date_1_3
-			,@RequestParam("date_2_1") String date_2_1,@RequestParam("date_2_2") String date_2_2
-			,@RequestParam("date_2_3") String date_2_3,@RequestParam("date_3_1") String date_3_1
-			,@RequestParam("date_3_2") String date_3_2,@RequestParam("date_3_3") String date_3_3
-			,@RequestParam("address") String address,@RequestParam("shops") String shops
-			,@RequestParam("pid") long pid,@PathVariable("insureAmount") final String insureAmountS,
+	public AjaxResponse<RecipientEntity> insert(final Model model, @RequestParam("name") String name,
+			@RequestParam("age") String age, @RequestParam("gender") String gender, @RequestParam("tel") String tel,
+			@RequestParam("date_1_1") String date_1_1, @RequestParam("date_1_2") String date_1_2,
+			@RequestParam("date_1_3") String date_1_3, @RequestParam("date_2_1") String date_2_1,
+			@RequestParam("date_2_2") String date_2_2, @RequestParam("date_2_3") String date_2_3,
+			@RequestParam("date_3_1") String date_3_1, @RequestParam("date_3_2") String date_3_2,
+			@RequestParam("date_3_3") String date_3_3, @RequestParam("address") String address,
+			@RequestParam("pid") long pid, @PathVariable("insureAmount") final String insureAmountS,
 			@PathVariable("premiumAfterDiscount") final String premiumAfterDiscountS,
 			@PathVariable("getPoint") final String getPointS) {
-		
+
 		AjaxResponse<RecipientEntity> response = new AjaxResponse<RecipientEntity>();
 		try {
 			RecipientEntity form = new RecipientEntity();
-			ProductEntity productEntity = productService.getOne(pid);
-			
+			ProductEntity productEntity = productService.getOneAll(pid);
+
 			productEntity.setInsureAmount(BigDecimal.valueOf(Double.parseDouble(insureAmountS)));
 			productEntity.setPremiumAfterDiscount(BigDecimal.valueOf(Double.parseDouble(premiumAfterDiscountS)));
 			productEntity.setGetPoint(BigDecimal.valueOf(Double.parseDouble(getPointS)));
@@ -132,38 +135,40 @@ public class AdminRecipientController {
 			form.setGender(gender);
 			form.setAge(Integer.parseInt(age));
 			form.setTel(tel);
-			form.setBookedTime_1(date_1_1+" "+date_1_2+" "+date_1_3);
-			form.setBookedTime_2(date_2_1+" "+date_2_2+" "+date_2_3);
-			form.setBookedTime_3(date_3_1+" "+date_3_2+" "+date_3_3);
+			form.setBookedTime_1(date_1_1 + " " + date_1_2 + " " + date_1_3);
+			form.setBookedTime_2(date_2_1 + " " + date_2_2 + " " + date_2_3);
+			form.setBookedTime_3(date_3_1 + " " + date_3_2 + " " + date_3_3);
 			form.setProduct(productEntity);
 			ConvenienceStoreEntity convenienceStoreEntity = convenienceStoreDao.findByAddress(address);
 			form.setConvenienceStoreEntity(convenienceStoreEntity);
 			form.setCreatedTime(new Timestamp(new Date().getTime()));
-			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			log.debug("userDetails: {}",userDetails);
+			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+					.getPrincipal();
+			log.debug("userDetails: {}", userDetails);
 			form.setCreatedBy(userDetails.getUsername());
 			final RecipientEntity insertResult = recipientService.insert(form);
 			response.setData(insertResult);
-		
+
 		} catch (final ApplicationException ex) {
 			ex.printStackTrace();
 			response.addMessages(ex.getMessages());
 		} catch (final Exception e) {
 			response.addException(e);
 		}
-		
+
 		log.debug("{}", response);
-		
+
 		return response;
 	}
 
 	@RequestMapping(method = RequestMethod.PUT)
 	@ResponseBody
-	public AjaxResponse<RecipientEntity> update(final Model model,@RequestParam("pid")long pid,@RequestParam(name="userName") String userName,@RequestBody final RecipientEntity form) {
-		
+	public AjaxResponse<RecipientEntity> update(final Model model, @RequestParam("pid") long pid,
+			@RequestParam(name = "userName") String userName, @RequestBody final RecipientEntity form) {
+
 		log.debug("{}", form);
 		final AjaxResponse<RecipientEntity> response = new AjaxResponse<RecipientEntity>();
-		
+
 		try {
 			ProductEntity productEntity = productService.getOne(pid);
 			UserEntity user = userDao.findByName(userName);
@@ -171,13 +176,14 @@ public class AdminRecipientController {
 			form.setProduct(productEntity);
 			System.out.println(form);
 			form.setModifiedTime(new Timestamp(new Date().getTime()));
-			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			log.debug("userDetails: {}",userDetails);
+			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+					.getPrincipal();
+			log.debug("userDetails: {}", userDetails);
 			form.setModifiedBy(userDetails.getUsername());
 			final RecipientEntity updateResult = recipientService.update(form);
 
 			response.setData(updateResult);
-			
+
 		} catch (final Exception e) {
 			response.addException(e);
 		}
@@ -188,20 +194,18 @@ public class AdminRecipientController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public AjaxResponse<RecipientEntity> delete(@PathVariable(value = "id") final long id) {
-		
+
 		log.debug("{}", id);
-		
+
 		final AjaxResponse<RecipientEntity> response = new AjaxResponse<RecipientEntity>();
-		
-		try {			
+
+		try {
 			recipientService.delete(id);
-		
-			
+
 		} catch (final Exception e) {
 			return new AjaxResponse<>(e);
 		}
 		return response;
-		
-				
+
 	}
 }
