68d096b5102c1fc7cb13c0fff4cecfe2f8ba8b9a
hilda.chang <hilda.chang@softleader.com.tw>
2017-10-11 18:18:22 +0800

    -xms 512 -> 768 add -xmx 768

--- run-auto.bat
+++ run-auto.bat
@@ -1,3 +1,3 @@
 set war=triplei-portal-0.0.1-SNAPSHOT.war
-java -jar -Xms512m -Xss512m -Dfile.encoding=UTF-8 target\%war% --spring.jpa.hibernate.ddl-auto=update
+java -jar -Xms768m -Xmx768m -Xss512m -Dfile.encoding=UTF-8 target\%war% --spring.jpa.hibernate.ddl-auto=update
 goto exit
