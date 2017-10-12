aa5b0ec3cb63bb705921d925efa030f1c4804b27
hilda.chang <hilda.chang@softleader.com.tw>
2017-10-12 15:55:29 +0800

    no message

--- run-auto.bat
+++ run-auto.bat
@@ -1,3 +1,3 @@
 set war=triplei-portal-0.0.1-SNAPSHOT.war
-java -jar -Xms768m -Xmx768m -Xss512m -Dfile.encoding=UTF-8 target\%war% --spring.jpa.hibernate.ddl-auto=update
+java -jar -Xms512m -Xmx512m -Dfile.encoding=UTF-8 -Dupload.location=C:\Temp -jar target\%war% --spring.jpa.hibernate.ddl-auto=update
 goto exit
